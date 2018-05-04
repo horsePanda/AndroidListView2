ff545ea932006699d3e68474faea7b94025dd57e
huangxiaoyu <huangxiaoyuonly@gmail.com>
2018-05-03 23:18:10 -0700

    add edit text and add new item with random image

--- app/src/main/java/com/example/huangxiaoyu/listviewassignment2/CustomListAdapter.java
+++ app/src/main/java/com/example/huangxiaoyu/listviewassignment2/CustomListAdapter.java
@@ -8,6 +8,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class CustomListAdapter extends ArrayAdapter {
     //to reference the Activity
     private final Activity _context;
@@ -16,27 +19,37 @@ public class CustomListAdapter extends ArrayAdapter {
     private final Integer[] _imageIDArray;
 
     //to store the list of countries
-    private final String[] _nameArray;
+    private ArrayList<String> _comment;
+    private ArrayList<Integer> _imageNumber = new ArrayList<>();
 
     //to store the list of countries
 
-    public CustomListAdapter(Activity context, String[] nameArray, Integer[] imageIDArray) {
-        super(context, R.layout.listview_row, nameArray);
+    public CustomListAdapter(Activity context, ArrayList<String> comment, Integer[] imageIDArray) {
+        super(context, R.layout.listview_row, comment);
         this._context = context;
         this._imageIDArray = imageIDArray;
-        this._nameArray = nameArray;
+        this._comment = comment;
     }
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater=_context.getLayoutInflater();
         View rowView=inflater.inflate(R.layout.listview_row, null,true);
+        Random randomNumberGenerator = new Random();
+        //this is the upper bound of our generator, it will generate int number from 0 - 5
+        int number = randomNumberGenerator.nextInt(6);
 
-        //this code gets references to objects in the listview_row.xml file
-        TextView nameTextField = (TextView) rowView.findViewById(R.id.nameTextViewID);
+        //this code gets references to objects in the listView_row.xml file
+        TextView commentTextField = (TextView) rowView.findViewById(R.id.commentTextViewID);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView1ID);
 
         //this code sets the values of the objects to values from the arrays
-        nameTextField.setText(_nameArray[position]);
-        imageView.setImageResource(_imageIDArray[position]);
+        commentTextField.setText(_comment.get(position));
+        if (_imageNumber.size() == 0 || _imageNumber.size() - 1 < position) {
+            imageView.setImageResource(_imageIDArray[number]);
+            _imageNumber.add(number);
+        } else {
+            imageView.setImageResource(_imageIDArray[_imageNumber.get(position)]);
+        }
+
 
         return rowView;
 
--- app/src/main/java/com/example/huangxiaoyu/listviewassignment2/MainActivity.java
+++ app/src/main/java/com/example/huangxiaoyu/listviewassignment2/MainActivity.java
@@ -1,53 +1,79 @@
 package com.example.huangxiaoyu.listviewassignment2;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] nameArray = {
-            "Base Jump",
-            "Bungee Jumping",
-            "Diving",
-            "Ski Jumping",
-            "Paragliding",
-            "Trampoline",
-            "VolleyBall",
-            "SkyDiving",
-            "Yoga" };
 
-    private String[] infoArray = {
-            "this is the basic sports",
-            "It is very popular for adventures",
-            "you can explore a new world",
-            "you have to control yourself",
-            "It is like a fly",
-            "It is interesting but need practice",
-            "It is popular in colleage",
-            "It is a little bit scary but worth it",
-            "It is strench your muscle "
-    };
-
+    private EditText commentText;
+    private Button addButton;
     private Integer[] imageArray = {
-            R.drawable.base_jumping96,
-            R.drawable.bungee_jumping,
-            R.drawable.diving_96,
-            R.drawable.ski_jumping_96,
-            R.drawable.paragliding_96,
-            R.drawable.trampoline_96,
-            R.drawable.volleyball_player_96,
-            R.drawable.skydive_96,
-            R.drawable.yoga_96
+            R.drawable.apple_icon,
+            R.drawable.banana_icon,
+            R.drawable.lemon_icon,
+            R.drawable.peach_icon,
+            R.drawable.pineapple_icon,
+            R.drawable.rose_icon,
     };
     private ListView listView;
+    private ArrayList<String> commentArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CustomListAdapter sportsListView = new CustomListAdapter(this, nameArray,imageArray);
+        //set adaptor
+        commentArray = new ArrayList<>();
+        commentArray.add("this is an example ^-^");
+        final CustomListAdapter sportsListViewAdaptor = new CustomListAdapter(this, commentArray, imageArray);
         listView = (ListView) findViewById(R.id.listViewID);
-        listView.setAdapter(sportsListView);
+        listView.setAdapter(sportsListViewAdaptor);
+
+        commentText = findViewById(R.id.editTextID);
+        addButton = findViewById(R.id.addButtonID);
+        //set button click listener
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userInput = commentText.getText().toString();
+
+                //if input is empty
+                if (TextUtils.isEmpty(commentText.getText())) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                    alertDialog.setTitle("Alert");
+                    alertDialog.setMessage("there must be something you want to say");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                } else {
+                    commentArray.add(userInput);
+                    sportsListViewAdaptor.notifyDataSetChanged();
+                }
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                if (imm.isAcceptingText()) { // verify if the soft keyboard is open
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                }
+            }
+        });
 
     }
 }
+
+
